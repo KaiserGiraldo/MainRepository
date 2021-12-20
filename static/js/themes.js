8789f2b281aefb43fbac6da21167d007c98247df
KaiserGiraldo <kaiserjuanmiguelgiraldo@gmail.com>
2021-12-20 13:18:20 -0500

    1

--- static/js/themes.js
+++ static/js/themes.js
@@ -1,118 +1,183 @@
-var themes = [ {
-  file : 'yotsuba.css',
-  label : 'Yotsuba',
-  id : 'yotsuba'
-}, {
-  file : 'yotsuba_b.css',
-  label : 'Yotsuba B',
-  id : 'yotsuba b'
-}, {
-  file : 'proton.css',
-  label : 'Proton',
-  id : 'proton'
-}, {
-  file : 'happyday.css',
-  label : 'Happy Day',
-  id : 'happyday'
-} ];
+var theme = {};
 
-var addedTheme;
+theme.themes = [ {
+      label : '掲示板固有のCSS',
+      id : 'custom'
+  }, {
+      label : 'Yotsuba B',
+      id : 'yotsuba_b'
+  }, {
+      label : 'Yotsuba P',
+      id : 'yotsuba_p'
+  }, {
+      label : 'Yotsuba',
+      id : 'yotsuba'
+  }, {
+      label : 'Miku',
+      id : 'miku'
+  }, {
+      label : 'Warosu',
+      id : 'warosu'
+  }, {
+      label : 'Vivian',
+      id : 'vivian'
+  }, {
+      label : 'Tomorrow',
+      id : 'tomorrow'
+  }, {
+      label : 'Lain',
+      id : 'lain'
+  }, {
+      label : 'Royal',
+      id : 'royal'
+  }, {
+      label : 'Redchanit',
+      id : 'redchanit'
+  }, {
+/*
+      label : 'Sonic 3 & Knuckles',
+      id : 'sonic3'
+  }, {
+      label : 'Final Fantasy',
+      id : 'finalfantasy'
+  }, {
+*/
+      label : 'ModChannel OS8',
+      id : 'moeos'
+  }, {
+      label : 'ModChannel 95',
+      id : 'win95'
+  }, {
+      label : 'Penumbra',
+      id : 'penumbra'
+  }, {
+      label : 'Penumbra (Clear)',
+      id : 'penumbra_clear'
+  }
+];
 
-function updateCss() {
+theme.themeLink = undefined;
+theme.globalTheme = undefined;
+theme.customCssHref = "";
 
-  if (addedTheme) {
-    addedTheme.parentNode.removeChild(addedTheme);
-    addedTheme = null;
+theme.addThemeSelector = function() {
+
+  var postingLink = document.getElementById('navMod');
+
+  if (!postingLink) {
+    return
   }
 
-  for (var i = 0; i < themes.length; i++) {
-    var theme = themes[i];
-
+  var referenceNode = postingLink.previousSibling.previousSibling;
+ 
+  postingLink.parentNode.insertBefore(document.createTextNode(' '),
+      referenceNode);
+ 
+  var divider = document.createElement('span');
+  divider.innerHTML = '/';
+  postingLink.parentNode.insertBefore(divider, referenceNode);
+ 
+  postingLink.parentNode.insertBefore(document.createTextNode(' '),
+      referenceNode);
+ 
+  var themeSelector = document.createElement('select');
+  themeSelector.id = 'themeSelector';
+ 
+  /*
+  var vanillaOption = document.createElement('option');
+  vanillaOption.innerHTML = 'Default';
+  themeSelector.appendChild(vanillaOption);
+  */
+ 
+  theme.themes.forEach((theme) => {
+ 
+    var themeOption = document.createElement('option');
+    themeOption.innerHTML = theme.label;
+ 
     if (theme.id === localStorage.selectedTheme) {
-      addedTheme = theme.element;
-      document.head.appendChild(theme.element);
+      themeOption.selected = true;
+    }
+ 
+    themeSelector.appendChild(themeOption);
+ 
+  })
+ 
+  themeSelector.onchange = function() {
+ 
+    var selectedTheme = theme.themes[themeSelector.selectedIndex];
+ 
+    if (selectedTheme.id === localStorage.selectedTheme) {
+      return;
+    }
+ 
+    localStorage.selectedTheme = selectedTheme.id;
+ 
+    theme.load();
+ 
+  };
+ 
+  postingLink.parentNode.insertBefore(themeSelector, referenceNode);
+ 
+  postingLink.parentNode.insertBefore(document.createTextNode(' '),
+      referenceNode);
+
+};
+
+theme.load = function(init) {
+
+  var currentTheme = localStorage.selectedTheme;
+
+  //loading a custom theme
+  if (currentTheme === 'custom') {
+    // document.body.className = 'theme_board'; //shouldn't be necessary anymore
+    theme.themeLink.href = theme.customCssHref;
+    if (theme.globalTheme !== undefined) {
+      document.head.insertBefore(theme.globalTheme, document.getElementById("nullSyntax"));
+    }
+  //loading a non-custom theme
+  } else {
+    // document.body.className = 'theme_' + currentTheme;
+    theme.themeLink.href = '/.static/css/' + currentTheme + '.css';
+    if (theme.globalTheme !== undefined) {
+      theme.globalTheme.remove();
+    }
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.body.classList.add("jsenabled");
+
+  var ending = '/custom.css';
+  
+  //if there's a `link` element with "custom.css", then remember it
+  var loadedCss = document.getElementsByTagName('link');
+  for (var i in Array.from(loadedCss)) {
+	var cssHref = loadedCss[i].href;
+  
+    if (cssHref !== undefined && cssHref.indexOf(ending) === 
+	cssHref.length - ending.length) {
+      theme.customCssHref = cssHref;
+      loadedCss[i].remove();
+      break;
+    }
+
+    if (loadedCss[i].className === "globalTheme") {
+      theme.globalTheme = loadedCss[i];
     }
   }
 
-}
+  theme.themeLink = document.createElement("link")
+  theme.themeLink.type = "text/css";
+  theme.themeLink.rel = "stylesheet";
+  theme.themeLink.id = "themeLink";
+  document.head.insertBefore(theme.themeLink, document.getElementById("nullSyntax"));
 
-if (!DISABLE_JS) {
+  var currentTheme = localStorage.selectedTheme;
+  localStorage.selectedTheme = currentTheme === undefined ?
+	theme.themes[0].id : currentTheme;
 
-  for (var i = 0; i < themes.length; i++) {
-    themes[i].element = document.createElement('link');
-    themes[i].element.type = 'text/css';
-    themes[i].element.rel = 'stylesheet';
-    themes[i].element.href = '/.static/css/' + themes[i].file;
-  }
+  theme.addThemeSelector();
 
-  updateCss();
+  theme.load(true);
+});
 
-  var postingLink = document.getElementById('navPosting');
-
-  if (postingLink) {
-
-    var referenceNode = postingLink.nextSibling;
-
-    postingLink.parentNode.insertBefore(document.createTextNode(''),
-        referenceNode);
-
-    var divider = document.createElement('span');
-    divider.innerHTML = '/&nbsp;';
-    postingLink.parentNode.insertBefore(divider, referenceNode);
-
-    postingLink.parentNode.insertBefore(document.createTextNode(''),
-        referenceNode);
-
-    var themeSelector = document.createElement('select');
-    themeSelector.id = 'themeSelector';
-
-    var vanillaOption = document.createElement('option');
-    vanillaOption.innerHTML = 'Tomorrow';
-    themeSelector.appendChild(vanillaOption);
-
-    for (i = 0; i < themes.length; i++) {
-
-      var theme = themes[i];
-
-      var themeOption = document.createElement('option');
-      themeOption.innerHTML = theme.label;
-
-      if (theme.id === localStorage.selectedTheme) {
-        themeOption.selected = true;
-      }
-
-      themeSelector.appendChild(themeOption);
-
-    }
-
-    themeSelector.onchange = function() {
-
-      if (!themeSelector.selectedIndex) {
-
-        if (localStorage.selectedTheme) {
-
-          delete localStorage.selectedTheme;
-
-          updateCss();
-        }
-
-        return;
-      }
-
-      var selectedTheme = themes[themeSelector.selectedIndex - 1];
-
-      if (selectedTheme.id === localStorage.selectedTheme) {
-        return;
-      }
-
-      localStorage.selectedTheme = selectedTheme.id;
-
-      updateCss();
-
-    };
-
-    postingLink.parentNode.insertBefore(themeSelector, referenceNode);
-
-  }
-
-}
