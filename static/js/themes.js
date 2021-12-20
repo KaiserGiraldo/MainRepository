2793004cf7cb1a221a4129dd1fd84489bb613d22
KaiserGiraldo <kaiserjuanmiguelgiraldo@gmail.com>
2021-12-20 13:26:42 -0500

    1

--- static/js/themes.js
+++ static/js/themes.js
@@ -1,183 +1,78 @@
-var theme = {};
+var themes = {};
 
-theme.themes = [ {
-      label : '掲示板固有のCSS',
-      id : 'custom'
-  }, {
-      label : 'Yotsuba B',
-      id : 'yotsuba_b'
-  }, {
-      label : 'Yotsuba P',
-      id : 'yotsuba_p'
-  }, {
-      label : 'Yotsuba',
-      id : 'yotsuba'
-  }, {
-      label : 'Miku',
-      id : 'miku'
-  }, {
-      label : 'Warosu',
-      id : 'warosu'
-  }, {
-      label : 'Vivian',
-      id : 'vivian'
-  }, {
-      label : 'Tomorrow',
-      id : 'tomorrow'
-  }, {
-      label : 'Lain',
-      id : 'lain'
-  }, {
-      label : 'Royal',
-      id : 'royal'
-  }, {
-      label : 'Redchanit',
-      id : 'redchanit'
-  }, {
-/*
-      label : 'Sonic 3 & Knuckles',
-      id : 'sonic3'
-  }, {
-      label : 'Final Fantasy',
-      id : 'finalfantasy'
-  }, {
-*/
-      label : 'ModChannel OS8',
-      id : 'moeos'
-  }, {
-      label : 'ModChannel 95',
-      id : 'win95'
-  }, {
-      label : 'Penumbra',
-      id : 'penumbra'
-  }, {
-      label : 'Penumbra (Clear)',
-      id : 'penumbra_clear'
-  }
-];
+themes.init = function() {
 
-theme.themeLink = undefined;
-theme.globalTheme = undefined;
-theme.customCssHref = "";
+  themes.themes = [ {
+    label : 'Clear',
+    id : 'clear'
+  } ];
 
-theme.addThemeSelector = function() {
+  var postingLink = document.getElementById('navPosting');
 
-  var postingLink = document.getElementById('navMod');
+  if (postingLink) {
 
-  if (!postingLink) {
-    return
-  }
+    var referenceNode = postingLink.nextSibling;
 
-  var referenceNode = postingLink.previousSibling.previousSibling;
- 
-  postingLink.parentNode.insertBefore(document.createTextNode(' '),
-      referenceNode);
- 
-  var divider = document.createElement('span');
-  divider.innerHTML = '/';
-  postingLink.parentNode.insertBefore(divider, referenceNode);
- 
-  postingLink.parentNode.insertBefore(document.createTextNode(' '),
-      referenceNode);
- 
-  var themeSelector = document.createElement('select');
-  themeSelector.id = 'themeSelector';
- 
-  /*
-  var vanillaOption = document.createElement('option');
-  vanillaOption.innerHTML = 'Default';
-  themeSelector.appendChild(vanillaOption);
-  */
- 
-  theme.themes.forEach((theme) => {
- 
-    var themeOption = document.createElement('option');
-    themeOption.innerHTML = theme.label;
- 
-    if (theme.id === localStorage.selectedTheme) {
-      themeOption.selected = true;
+    postingLink.parentNode.insertBefore(document.createTextNode(' '),
+        referenceNode);
+
+    var divider = document.createElement('span');
+    divider.innerHTML = '/';
+    postingLink.parentNode.insertBefore(divider, referenceNode);
+
+    postingLink.parentNode.insertBefore(document.createTextNode(' '),
+        referenceNode);
+
+    var themeSelector = document.createElement('select');
+    themeSelector.id = 'themeSelector';
+
+    var vanillaOption = document.createElement('option');
+    vanillaOption.innerHTML = 'Default';
+    themeSelector.appendChild(vanillaOption);
+
+    for (var i = 0; i < themes.themes.length; i++) {
+
+      var theme = themes.themes[i];
+
+      var themeOption = document.createElement('option');
+      themeOption.innerHTML = theme.label;
+
+      if (theme.id === localStorage.selectedTheme) {
+        themeOption.selected = true;
+      }
+
+      themeSelector.appendChild(themeOption);
+
     }
- 
-    themeSelector.appendChild(themeOption);
- 
-  })
- 
-  themeSelector.onchange = function() {
- 
-    var selectedTheme = theme.themes[themeSelector.selectedIndex];
- 
-    if (selectedTheme.id === localStorage.selectedTheme) {
-      return;
-    }
- 
-    localStorage.selectedTheme = selectedTheme.id;
- 
-    theme.load();
- 
-  };
- 
-  postingLink.parentNode.insertBefore(themeSelector, referenceNode);
- 
-  postingLink.parentNode.insertBefore(document.createTextNode(' '),
-      referenceNode);
+
+    themeSelector.onchange = function() {
+
+      if (!themeSelector.selectedIndex) {
+
+        if (localStorage.selectedTheme) {
+          delete localStorage.selectedTheme;
+          themeLoader.load();
+        }
+
+        return;
+      }
+
+      var selectedTheme = themes.themes[themeSelector.selectedIndex - 1];
+
+      if (selectedTheme.id === localStorage.selectedTheme) {
+        return;
+      }
+
+      localStorage.selectedTheme = selectedTheme.id;
+
+      themeLoader.load();
+
+    };
+
+    postingLink.parentNode.insertBefore(themeSelector, referenceNode);
+
+  }
 
 };
 
-theme.load = function(init) {
-
-  var currentTheme = localStorage.selectedTheme;
-
-  //loading a custom theme
-  if (currentTheme === 'custom') {
-    // document.body.className = 'theme_board'; //shouldn't be necessary anymore
-    theme.themeLink.href = theme.customCssHref;
-    if (theme.globalTheme !== undefined) {
-      document.head.insertBefore(theme.globalTheme, document.getElementById("nullSyntax"));
-    }
-  //loading a non-custom theme
-  } else {
-    // document.body.className = 'theme_' + currentTheme;
-    theme.themeLink.href = '/.static/css/' + currentTheme + '.css';
-    if (theme.globalTheme !== undefined) {
-      theme.globalTheme.remove();
-    }
-  }
-};
-
-document.addEventListener("DOMContentLoaded", function() {
-  document.body.classList.add("jsenabled");
-
-  var ending = '/custom.css';
-  
-  //if there's a `link` element with "custom.css", then remember it
-  var loadedCss = document.getElementsByTagName('link');
-  for (var i in Array.from(loadedCss)) {
-	var cssHref = loadedCss[i].href;
-  
-    if (cssHref !== undefined && cssHref.indexOf(ending) === 
-	cssHref.length - ending.length) {
-      theme.customCssHref = cssHref;
-      loadedCss[i].remove();
-      break;
-    }
-
-    if (loadedCss[i].className === "globalTheme") {
-      theme.globalTheme = loadedCss[i];
-    }
-  }
-
-  theme.themeLink = document.createElement("link")
-  theme.themeLink.type = "text/css";
-  theme.themeLink.rel = "stylesheet";
-  theme.themeLink.id = "themeLink";
-  document.head.insertBefore(theme.themeLink, document.getElementById("nullSyntax"));
-
-  var currentTheme = localStorage.selectedTheme;
-  localStorage.selectedTheme = currentTheme === undefined ?
-	theme.themes[0].id : currentTheme;
-
-  theme.addThemeSelector();
-
-  theme.load(true);
-});
-
+themes.init();
--- static/js/thread.js
+++ static/js/thread.js
@@ -1,16 +1,16 @@
 var thread = {};
 
-var active_refresh_time = 10;
-
 thread.init = function() {
 
   api.mod = !!document.getElementById('divMod');
 
   api.hiddenCaptcha = !document.getElementById('captchaDiv');
 
+  document.getElementById('checkboxChangeRefresh').onchange = thread.changeRefresh;
+
   document.getElementsByTagName('body')[0].onscroll = function() {
 
-    if (!thread.unreadPosts || !thread.lastPost) {
+    if (!thread.unreadPosts) {
       return;
     }
 
@@ -27,21 +27,21 @@ thread.init = function() {
   api.boardUri = document.getElementById('boardIdentifier').value;
   thread.divPosts = document.getElementsByClassName('divPosts')[0];
 
+  thread.initThread();
+
   document.getElementsByClassName('divRefresh')[0].style.display = 'block';
 
   thread.messageLimit = +document.getElementById('labelMessageLength').innerHTML;
   thread.refreshLabel = document.getElementById('labelRefresh');
-  thread.wsStatusLabel = document.getElementById('labelWsStatus');
-  thread.autoIndicator = document.getElementById('labelAutoIndicator');
 
   thread.refreshButton = document.getElementById('refreshButton');
-  thread.lastRefresh = 0;
 
-  thread.initThread();
+  thread.refreshButton.onclick = function() {
+    thread.refreshPosts(true)
+  };
 
-  //TODO: most of these would be better-replaced by the parent form's onsubmit
   if (document.getElementById('divArchive')) {
-    api.convertButton('archiveFormButton', thread.archiveThread, 'archiveField');
+    api.convertButton('archiveFormButon', thread.archiveThread, 'archiveField');
   }
 
   if (document.getElementById('divMerge')) {
@@ -50,7 +50,7 @@ thread.init = function() {
 
   if (document.getElementById('controlThreadIdentifier')) {
 
-    api.convertButton('settingsFormButton', thread.saveThreadSettings,
+    api.convertButton('settingsFormButon', thread.saveThreadSettings,
         'threadSettingsField');
 
     if (document.getElementById('ipDeletionForm')) {
@@ -62,28 +62,17 @@ thread.init = function() {
       api.convertButton('transferFormButton', thread.transfer, 'transferField');
     }
 
-    api.convertButton('inputBan', postCommon.banPosts, 'banField');
-    api.convertButton('inputIpDelete', postCommon.deleteFromIpOnBoard);
-    api.convertButton('inputThreadIpDelete', postCommon.deleteFromIpOnThread);
-    api.convertButton('inputSpoil', postCommon.spoilFiles);
+    api.convertButton('inputBan', posting.banPosts, 'banField');
+    api.convertButton('inputIpDelete', posting.deleteFromIpOnBoard);
+    api.convertButton('inputThreadIpDelete', posting.deleteFromIpOnThread);
+    api.convertButton('inputSpoil', posting.spoilFiles);
 
   }
 
-  //TODO (see above): ...like this:
   thread.replyButton = document.getElementById('formButton');
-  thread.postingForm = document.getElementById('postingFormContents').parentNode;
+  thread.replyButton.disabled = false;
 
-  thread.postingForm.onsubmit = function(e) {
-    e.preventDefault()
-    thread.postReply();
-  }
-
-  var archiveLinks = document.getElementsByClassName('archiveLinkThread');
-  for (var i = 0; i < archiveLinks.length; i++) {
-    var archiveLink = archiveLinks[i];
-    archiveLink.href = 'http://archive.today/?run=1&url='+encodeURIComponent(document.location);
-    archiveLink.parentNode.style.display = 'inline-block';
-  }
+  api.convertButton(thread.replyButton, thread.postReply);
 
   var replies = document.getElementsByClassName('postCell');
 
@@ -110,17 +99,11 @@ thread.init = function() {
 
       });
 
-  document.addEventListener("visibilitychange", function() {
-    if (document.hidden) {
-       thread.lastRefresh = 600;
-    } else {
-	thread.lastRefresh = active_refresh_time;
-	thread.currentRefresh = active_refresh_time;
-    }
-  })
+  var postingQuotes = document.getElementsByClassName('linkQuote');
 
-  //initial refresh to update the count indicators
-  thread.refreshPosts();
+  for (var i = 0; i < postingQuotes.length; i++) {
+    thread.processPostingQuote(postingQuotes[i]);
+  }
 
 };
 
@@ -133,6 +116,14 @@ thread.initThread = function() {
   thread.expectedPosts = [];
   thread.lastReplyId = 0;
   thread.originalTitle = document.title;
+  posting.highLightedIds = [];
+  posting.idsRelation = {};
+
+  var ids = document.getElementsByClassName('labelId');
+
+  for (i = 0; i < ids.length; i++) {
+    posting.processIdLabel(ids[i]);
+  }
 
   thread.unreadPosts = 0;
   api.threadId = +document.getElementsByClassName('opCell')[0].id;
@@ -238,7 +229,6 @@ thread.archiveThread = function() {
 
       api.resetIndicators({
         locked : document.getElementsByClassName('lockIndicator').length,
-        bumplock : document.getElementsByClassName('bumpLockIndicator').length,
         pinned : document.getElementsByClassName('pinIndicator').length,
         cyclic : document.getElementsByClassName('cyclicIndicator').length,
         archived : true
@@ -255,7 +245,6 @@ thread.archiveThread = function() {
 thread.saveThreadSettings = function() {
 
   var pinned = document.getElementById('checkboxPin').checked;
-  var bumplock = document.getElementById('checkboxBumplock').checked;
   var locked = document.getElementById('checkboxLock').checked;
   var cyclic = document.getElementById('checkboxCyclic').checked;
 
@@ -264,8 +253,7 @@ thread.saveThreadSettings = function() {
     threadId : api.threadId,
     pin : pinned,
     lock : locked,
-    cyclic : cyclic,
-    bumplock : bumplock
+    cyclic : cyclic
   }, function setLock(status, data) {
 
     if (status === 'ok') {
@@ -274,7 +262,6 @@ thread.saveThreadSettings = function() {
         locked : locked,
         pinned : pinned,
         cyclic : cyclic,
-        bumplock : bumplock,
         archived : document.getElementsByClassName('archiveIndicator').length
       });
 
@@ -290,14 +277,11 @@ thread.replyCallback = function(status, data) {
   if (status === 'ok') {
 
     postCommon.storeUsedPostingPassword(api.boardUri, api.threadId, data);
-    api.addYou(api.boardUri, data);
 
     document.getElementById('fieldMessage').value = '';
     document.getElementById('fieldSubject').value = '';
     qr.clearQRAfterPosting();
     postCommon.clearSelectedFiles();
- 
-    document.getElementById('footer').scrollIntoView();
 
     if (!thread.autoRefresh || !thread.socket) {
       thread.refreshPosts(true);
@@ -311,7 +295,7 @@ thread.replyCallback = function(status, data) {
 
 thread.replyCallback.stop = function() {
 
-  thread.replyButton.value = thread.originalButtonText;
+  thread.replyButton.innerHTML = thread.originalButtonText;
 
   qr.setQRReplyText(thread.originalButtonText);
 
@@ -323,9 +307,9 @@ thread.replyCallback.stop = function() {
 thread.replyCallback.progress = function(info) {
 
   if (info.lengthComputable) {
-    var newText = 'アップロード' + Math.floor((info.loaded / info.total) * 100)
+    var newText = 'アップロード ' + Math.floor((info.loaded / info.total) * 100)
         + '%';
-    thread.replyButton.value = newText;
+    thread.replyButton.innerHTML = newText;
 
     qr.setQRReplyText(newText);
   }
@@ -363,14 +347,7 @@ thread.refreshCallback = function(error, receivedData) {
 
   thread.wsPort = receivedData.wsPort;
   thread.wssPort = receivedData.wssPort;
-  if (!thread.socket || thread.socket.readyState > 1) { //still closed
-	thread.stopWs();
-  	thread.startWs();
-  }
-
-  if (typeof tooltips !== "undefined") {
-    tooltips.cacheData(receivedData);
-  }
+  tooltips.cacheData(receivedData);
 
   var posts = receivedData.posts;
 
@@ -428,29 +405,6 @@ thread.refreshCallback = function(error, receivedData) {
     }
   }
 
-  var counts = posts.reduce((acc, post) => {
-    if (acc.ids.indexOf(post.id) === -1)
-      acc.ids.push(post.id);
-  	acc.files += post.files.length;
-  	return acc;
-  }, {
-    files:	0,
-    ids: 	[]
-  })
-  
-  var postCount = document.getElementById('postCount')
-  postCount.innerHTML = posts.length;
-  var idCount = document.getElementById('idCount');
-  if (counts.ids) {
-    idCount.innerHTML = counts.ids.length;
-    idCount.style.display = "inline";
-  } else {
-    idCount.innerHTML = counts.ids.length;
-    idCount.style.display = "none";
-  }
-  document.getElementById('fileCount').innerHTML = counts.files;
-  postCount.parentNode.style.display = "inherit";
-
   if (thread.autoRefresh
       && !(!JSON.parse(localStorage.noWs || 'false') && (thread.wsPort || thread.wssPort))) {
     thread.startTimer(thread.manualRefresh || foundPosts ? 5
@@ -465,7 +419,7 @@ thread.refreshCallback.stop = function() {
 
   thread.refreshingThread = false;
 
-  if (typeof sideCatalog !== "undefined" && sideCatalog.waitingForRefreshData) {
+  if (sideCatalog.waitingForRefreshData) {
     sideCatalog.loadThread(sideCatalog.waitingForRefreshData.cell,
         sideCatalog.waitingForRefreshData.thread);
     delete sideCatalog.waitingForRefreshData;
@@ -524,29 +478,24 @@ thread.sendReplyData = function(files, captchaId) {
   var typedPassword = document.getElementById('fieldPostingPassword').value
       .trim();
 
-  if (!postCommon.belowMaxFileSize(files)) {
-    alert("アップロードに失敗しました：ファイルが大きすぎます");
-    return;
-  }
-
   if (!typedMessage.length && !files.length) {
-    alert('メッセージまたはファイルは必須です。');
+    alert('A message or a file is mandatory.');
     return;
   } else if (!forcedAnon && typedName.length > 32) {
-    alert('名前が長すぎます。32文字未満にしてください。');
+    alert('名前が長すぎる、32文字以内にしてください。');
     return;
   } else if (typedMessage.length > thread.messageLimit) {
     alert('メッセージが長すぎます。' + thread.messageLimit
-        + '文字数以下に抑えてください');
+        + '文字以下に抑えてください。');
     return;
   } else if (typedEmail.length > 64) {
-    alert('電子メールが長すぎるため、64文字未満にしてください。');
+    alert('電子メールが長すぎる、64文字以内にしてください。');
     return;
   } else if (typedSubject.length > 128) {
-    alert('件名が長すぎます。128文字未満にしてください。');
+    alert('件名が長すぎる、128文字以下にしてください。');
     return;
   } else if (typedPassword.length > 8) {
-    alert('パスワードが長すぎます。8文字未満にしてください。');
+    alert('パスワードが長すぎる。8文字以内であるべきだ。');
     return;
   }
 
@@ -557,12 +506,11 @@ thread.sendReplyData = function(files, captchaId) {
   localStorage.setItem('deletionPassword', typedPassword);
 
   var spoilerCheckBox = document.getElementById('checkboxSpoiler');
-  var sageCheckbox = document.getElementById('doSageCheckbox');
 
   var noFlagCheckBox = document.getElementById('checkboxNoFlag');
 
-  thread.originalButtonText = thread.replyButton.value;
-  thread.replyButton.innerHTML = '0％をアップロード';
+  thread.originalButtonText = thread.replyButton.innerHTML;
+  thread.replyButton.innerHTML = '0%アップロード';
   qr.setQRReplyText(thread.replyButton.innerHTML);
   thread.replyButton.disabled = true;
   qr.setQRReplyEnabled(false);
@@ -577,7 +525,6 @@ thread.sendReplyData = function(files, captchaId) {
     password : typedPassword,
     message : typedMessage,
     email : typedEmail,
-    sage : sageCheckbox ? sageCheckbox.checked : false,
     files : files,
     boardUri : api.boardUri,
     threadId : api.threadId
@@ -585,7 +532,6 @@ thread.sendReplyData = function(files, captchaId) {
 
 };
 
-//TODO
 thread.processFilesToPost = function(captchaId) {
 
   postCommon.newGetFilesToUpload(function gotFiles(files) {
@@ -604,7 +550,7 @@ thread.postReply = function() {
 
   if (typedCaptcha.length !== 6 && typedCaptcha.length !== 112) {
 
-    alert('キャプチャの長さは正確に6文字（クッキーがない場合は112文字）です。');
+    alert('CAPTCHAは6文字（クッキーがない場合は112文字）ちょうどの長さです。');
     return;
   }
 
@@ -650,19 +596,9 @@ thread.transition = function() {
 };
 
 thread.startTimer = function(time) {
-  if (thread.socket && thread.socket.readyState === thread.socket.OPEN) {
-    return;
-  }
-
-  if (thread.currentRefresh) {
-    clearInterval(thread.refreshTimer);
-  }
 
   if (time > 600) {
     time = 600;
-  }
-  if (!document.hidden) {
-    time = active_refresh_time;
   }
 
   thread.currentRefresh = time;
@@ -676,7 +612,7 @@ thread.startTimer = function(time) {
 
     thread.currentRefresh--;
 
-    if (thread.currentRefresh < 1) {
+    if (!thread.currentRefresh) {
       clearInterval(thread.refreshTimer);
       thread.refreshPosts();
       thread.refreshLabel.innerHTML = '';
@@ -704,29 +640,18 @@ thread.startWs = function() {
     return;
   }
 
-  if ((thread.wsPort || thread.wssPort) === undefined ) {
-    return;
-  }
+  var isOnion = window.location.hostname.endsWith('.onion');
 
-  var protocol = (thread.wssPort && location.protocol == 'https:') ? 'wss' : 'ws';
+  var protocol = (thread.wssPort && !isOnion) ? 'wss' : 'ws';
 
+  var portToUse = (thread.wssPort && !isOnion) ? thread.wssPort : thread.wsPort;
+  
   thread.socket = new WebSocket(protocol + '://' + window.location.hostname
-      + ':' + (location.protocol == 'https:' ? thread.wssPort : false || thread.wsPort));
+      + ':' + portToUse);
 
   thread.socket.onopen = function(event) {
     thread.socket.send(api.boardUri + '-' + api.threadId);
-    clearInterval(thread.refreshTimer);
-    thread.refreshLabel.innerHTML = '';
-
-	thread.wsStatusLabel.style.color = "green";
-    thread.wsStatusLabel.title = "Websocket OK";
   };
-
-  thread.socket.onclose = function() {
-    thread.wsStatusLabel.style.color = "red";
-    thread.wsStatusLabel.title = "WebSocketが閉じられ、再接続を試みました";
-    thread.changeRefresh();
-  }
 
   thread.socket.onmessage = function(message) {
 
@@ -754,29 +679,23 @@ thread.startWs = function() {
     }
     case 'delete': {
 
-      //filter duplicates
-      message.target.filter((val, ind, arr) => arr.indexOf(val) === ind)
-      .forEach((target) => {
+      for (var i = 0; i < message.target.length; i++) {
 
-        var post = document.getElementById(target);
+        var post = document.getElementById(message.target[i]);
 
         if (!post) {
-          return;
+          continue;
         }
 
         var info = post.getElementsByClassName('postInfo')[0];
 
         var deletedLabel = document.createElement('span');
-        deletedLabel.innerHTML = '（削除）';
+        deletedLabel.innerHTML = '(削除)';
 
         info.insertBefore(deletedLabel,
-          info.getElementsByClassName('linkName')[0]);
+            info.getElementsByClassName('linkName')[0]);
 
-        if (!thread.noAutohideDeleted) {
-          hiding.hidePost(post.getElementsByClassName("linkSelf")[0], true, true);
-        }
-        
-      });
+      }
 
       break;
     }
@@ -796,7 +715,6 @@ thread.startWs = function() {
 thread.changeRefresh = function() {
 
   thread.autoRefresh = document.getElementById('checkboxChangeRefresh').checked;
-  thread.noAutohideDeleted = JSON.parse(localStorage.noAutohideDeleted || "false");
 
   if (!thread.autoRefresh) {
     thread.refreshLabel.innerHTML = '';
@@ -806,16 +724,13 @@ thread.changeRefresh = function() {
     clearInterval(thread.refreshTimer);
   } else {
 
-    thread.startTimer(5); //will get canceled by onopen anyway
-
-    if (!JSON.parse(localStorage.noWs || 'false')) {
-      thread.wsStatusLabel.style.display = "inherit";
-      thread.autoIndicator.innerText = "ライブアップデート";
+    if (!JSON.parse(localStorage.noWs || 'false')
+        && (thread.wsPort || thread.wssPort)) {
       thread.startWs();
     } else {
-      thread.wsStatusLabel.style.display = "none";
-      thread.autoIndicator.innerText = "Auto";
+      thread.startTimer(5);
     }
+
   }
 
 };
@@ -840,7 +755,7 @@ thread.deleteFromIp = function() {
       document.getElementById('ipField').value = '';
       document.getElementById('fieldBoards').value = '';
 
-      alert('投稿が削除されました。');
+      alert('削除された投稿');
 
     } else {
       alert(status + ': ' + JSON.stringify(data));
